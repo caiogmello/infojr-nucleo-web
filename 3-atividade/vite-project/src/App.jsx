e845ad5f9b75b596ba14589f8cc777574f856a0c
caiogmello <caiogomedemello@gmail.com>
2023-05-18 11:39:07 -0300

    commit inicial, projeto vite criado e estruturação do projeto calculadora

---
+++ 3-atividade/vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+import Calculator from './components/Calculator'
+
+function App() {
+  return <div className="App">
+    <Calculator/>
+  </div>   
+}
+
+export default App
---
+++ 3-atividade/vite-project/src/components/Calculator.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../Calculator.css"
+export default function Calculator() {
+    return (
+        <div className="buttons">
+            <button className="grey-buttons">AC</button>
+            <button className="grey-buttons">+/-</button>
+            <button className="grey-buttons">%</button>
+            <button className="operations">/</button>
+            <button className="number">7</button>
+            <button className="number">8</button>
+            <button className="number">9</button>
+            <button className="operations">*</button>
+            <button className="number">4</button>
+            <button className="number">5</button>
+            <button className="number">6</button>
+            <button className="operations">-</button>
+            <button className="number">1</button>
+            <button className="number">2</button>
+            <button className="number">3</button>
+            <button className="operations">+</button>
+            <button className="number">0</button>
+            <button className="comma">,</button>
+            <button id="zero" className="operations">=</button>
+
+
+
+
+        </div>
+    )
+}
