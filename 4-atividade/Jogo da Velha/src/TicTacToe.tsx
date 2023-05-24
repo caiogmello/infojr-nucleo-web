9629daa091268f424c5d661dd880569db1b46b67
caiogmello <caiogomedemello@gmail.com>
2023-05-24 12:07:20 -0300

    dando inicio criando alguns botões e estilo

--- 4-atividade/Jogo da Velha/src/TicTacToe.tsx
+++ 4-atividade/Jogo da Velha/src/TicTacToe.tsx
@@ -1,6 +1,24 @@
+import "./TicTacToe.css"
+
 export default function TicTacToe() {
     return (
-        <div>
-        Ola mundo</div>
+        <main>
+            <h1>
+                Jogo da velha!
+            </h1>
+            <section className="game">
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+                <button className="square"></button>
+            </section>
+        </main>
+        
+      
     )
 }
