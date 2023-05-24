dfbb1f4777eda473fd4e01a94af9ff4cf8302ef6
caiogmello <caiogomedemello@gmail.com>
2023-05-24 11:18:39 -0300

    criação do arquivo react

---
+++ 4-atividade/Jogo da Velha/src/App.tsx
@@ -0,0 +1,9 @@
+import TicTacToe from "./TicTacToe" 
+
+function App() {
+  return  <div className="App">
+      <TicTacToe/>
+  </div>
+}
+
+export default App
---
+++ 4-atividade/Jogo da Velha/src/TicTacToe.tsx
@@ -0,0 +1,6 @@
+export default function TicTacToe() {
+    return (
+        <div>
+        Ola mundo</div>
+    )
+}
